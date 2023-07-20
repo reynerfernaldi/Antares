f7938820d3e2e1f507b39698f996c343b9d77656
Reyner Fernaldi <reynerfernaldi@gmail.com>
2023-07-20 18:07:28 +0700

    resolve tracker

--- app/src/main/java/com/telkom/antares/ui/TrackerFragment.kt
+++ app/src/main/java/com/telkom/antares/ui/TrackerFragment.kt
@@ -8,14 +8,21 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.telkom.antares.R
+import com.telkom.antares.adapter.TrackerAdapter
+import com.telkom.antares.data.dao.TrackerData
 import com.telkom.antares.databinding.FragmentOpeningBinding
 import com.telkom.antares.databinding.FragmentTrackerBinding
+import com.telkom.antares.viewmodel.TrackerViewModel
 
-class TrackerFragment : Fragment() {
+class TrackerFragment : Fragment(), TrackerAdapter.TrackersInterface  {
     private var _binding: FragmentTrackerBinding? = null
     private val binding get() = _binding!!
+    private val viewModel : TrackerViewModel by viewModels()
+    private lateinit var adapter : TrackerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +35,10 @@ class TrackerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        adapter = TrackerAdapter(this)
+        dataEmpty()
+
         val status = resources.getStringArray(R.array.Status)
         val location = resources.getStringArray(R.array.Location)
 
@@ -44,9 +55,25 @@ class TrackerFragment : Fragment() {
             spinnerLocation.adapter = adapterLocation
 
         }
+        binding.btnAdd.setOnClickListener{
+            findNavController().navigate(R.id.action_trackerFragment_to_addBeaconFragment)
+        }
 
         binding.llBack.setOnClickListener{
             findNavController().navigate(R.id.action_trackerFragment_to_dashboardFragment)
         }
+    }
+
+    private fun dataEmpty() {
+        binding.apply {
+            viewModel.getDataTrackers().observe(viewLifecycleOwner) {
+                adapter.setData(it)
+            }
+            rvTracker.adapter = adapter
+            rvTracker.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        }
+    }
+    override fun detailTracker(Trackers: TrackerData) {
+        findNavController().navigate(R.id.action_trackerFragment_to_mapsFragment)
     }
 }
